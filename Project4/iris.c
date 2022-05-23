462d39e0778af5c158c773b79d2eadc60946c9d9
K-Fujita-onkyo <m5261108.aizu@gmail.com>
2022-05-23 12:09:46 +0900

    updated 4

--- Project4/iris.c
+++ Project4/iris.c
@@ -22,81 +22,66 @@ Kazuki Fujita: m5261108
 #include <stdlib.h>
 #include <math.h>
 
-#define I 4
-#define M 3
-#define P 150
-#define alpha 0.5
-#define n_update 20
+#define  I             4
+#define  M             3
+#define  P             150
+#define  alpha         0.1
+#define  n_update      100
+
+void PrintResult(int);
+void InitializeWeight();
 
 double w[M][I];
 double x[P][I];
+// ={
+//   {5.9 3.2 4.8 1.8 },
+//   {0.707,0.707},
+//   {0.342,-0.9397},
+//   {0.6,0.8}
+//   };
 double y[M];
 
-/*************************************************************/
-/* Print out the result of the q-th iteration                */
-/*************************************************************/
-void PrintResult(int q)
-{
-	int m, i;
+//The main program
+int main(){
 
-	printf("\n\n");
-	printf("Results in the %d-th iteration: \n", q);
-	for (m = 0; m < M; m++)
-	{
-		for (i = 0; i < I; i++)
-			printf("%5f ", w[m][i]);
-		printf("\n");
-	}
-	printf("\n\n");
-}
-
-/*************************************************************/
-/* The main program                                          */
-/*************************************************************/
-int main()
-{
 	int m, m0, i, j, p, q;
 	double norm, s, s0;
+	double sum;
 
-	/* Initialization of the connection weights */
-
+	//input iris data
 	for(i=0;i<P;i++){
+		norm=0;
 		for(j=0;j<I;j++){
 			scanf("%lf",&x[i][j]);
+			norm+=x[i][j]*x[i][j];
 		}
+		norm=sqrt(norm);
+		for(j=0;j<I;j++){
+			x[i][j]/=norm;
+		}
+		printf("x[%d]={%lf, %lf, %lf, %lf}\n",i, x[i][0], x[i][1], x[i][2], x[i][3]);
 	}
 
-	for (m = 0; m < M; m++)
-	{
-		norm = 0;
-		for (i = 0; i < I; i++)
-		{
-			w[m][i] = (double)(rand() % 10001) / 10000.0 - 0.5;
-			norm += w[m][i] * w[m][i];
-		}
-		norm = sqrt(norm);
-		for (i = 0; i < I; i++)
-			w[m][i] /= norm;
-	}
-
+	//Initiarize weight
+	InitializeWeight();
 	PrintResult(0);
 
 	/* Unsupervised learning */
 
 	for (q = 0; q < n_update; q++){
 		for (p = 0; p < P; p++){
-			s0 = 0;
+			s0 = 100;
 			for (m = 0; m < M; m++){
 				s = 0;
-				for (i = 0; i < I; i++) s += fabs(x[p][i]-w[m][i]);
-				if (s > s0){
+				for (i = 0; i < I; i++) s += w[m][i]*x[p][i];
+				if (s < s0){
 					s0 = s;
 					m0 = m;
 				}
 			}
 			//Update the weight of the winner
 
-			printf("winner:%d\n",m0);
+		    //printf("winner:%d\n",m0);
 			for (i = 0; i < I; i++) w[m0][i] += alpha * (x[p][i] - w[m0][i]);
 
 			norm = 0;
@@ -105,9 +90,11 @@ int main()
 
 			norm = sqrt(norm);
 
-			for (i = 0; i < I; i++)w[m0][i] /= norm;
+			for (i = 0; i < I; i++){
+				w[m0][i] /= norm;
+			}
 		}
-		PrintResult(q);
+		//PrintResult(q);
 	}
 
 	/* Classify the training patterns */
@@ -124,5 +111,41 @@ int main()
 			}
 		}
 		printf("Pattern[%d] belongs to %d-th class\n", p, m0);
+	}
+}
+
+//Print out the result of the q-th iteration
+void PrintResult(int q){
+	int m, i;
+
+	printf("\n\n");
+	printf("Results in the %d-th iteration: \n", q);
+	for (m = 0; m < M; m++)
+	{
+		for (i = 0; i < I; i++)
+			printf("%5f ", w[m][i]);
+		printf("\n");
+	}
+	printf("\n\n");
+}
+
+//Initialize weights
+void InitializeWeight(){
+
+	int m, i;
+	double normalize;
+
+	for (m = 0; m < M; m++){
+
+		normalize = 0;
+
+		for (i = 0; i < I; i++){
+			w[m][i] = (double)(rand() %10001)/10000.0 -0.5;
+			normalize += w[m][i] * w[m][i];
+		}
+		normalize = sqrt(normalize); //geometric mean
+		for (i = 0; i < I; i++){
+			w[m][i] /= normalize; 
+		}
 	}
 }
